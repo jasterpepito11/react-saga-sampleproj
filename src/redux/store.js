c360f2b52f5fe4401c3f076a07646be8877dcda4
Joshua Lester Pepito <35251527+jasterpepito11@users.noreply.github.com>
2023-01-09 17:07:09 +0800

    removed unused imports

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,4 @@
-import { applyMiddleware, compose } from "redux";
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from './reducers/index';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from './sagas/index';
